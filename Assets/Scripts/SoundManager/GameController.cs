f62a9d40269850340a12479302706491ab3ba55d
Karl Ramstedt <a16karra@hs.local>
2017-01-30 12:17:59 +0100

    Initial sound manager

---
+++ Assets/Scripts/SoundManager/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public AudioSource horseSound;
+
+	void Start () {
+		SoundManager.instance.PlaySFX(horseSound);
+		SoundManager.instance.ResetVolume ();
+		Debug.Log(SoundManager.instance.globalVolume);
+	}
+	
+	void FixedUpdate () {
+		SoundManager.instance.ResetVolume ();
+	}
+}
---
+++ Assets/Scripts/SoundManager/VolumeManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeManager : MonoBehaviour{
+
+    [Range(0, 1)]
+    public float defaultGlobalVolume;
+    [Range(0, 1)]
+    public float defaultMusicVolume;
+    [Range(0, 1)]
+    public float defaultSFXVolume;
+
+    private float globalVolume;
+    private float musicVolume;
+    private float sFXVolume;
+
+    public void ResetVolume() {
+        globalVolume = defaultGlobalVolume;
+        musicVolume = defaultMusicVolume;
+        sFXVolume = defaultSFXVolume;
+    }
+
+    //if (volumeManager != null)
+    //    volumeManager.ResetVolume();
+    //else Debug.Log("Error: No volume manager added");
+}
