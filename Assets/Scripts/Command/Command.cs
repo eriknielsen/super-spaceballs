31db06a8d3e8f3d19da6280dd0c41dde42c4a0a8
eriknielsen <eriiik_@live.se>
2017-03-10 16:55:10 +0100

    improved some network stuff!

--- Assets/Scripts/Command/Command.cs
+++ Assets/Scripts/Command/Command.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public abstract class Command {
 
 	public enum AvailableCommands { None, Move, Push };
-	protected GameObject robot;
+	public GameObject robot;
 	//execute is called by the robot
 	public abstract void Execute();
 	public bool isFinished = false;
@@ -30,7 +30,6 @@ public class SerializableCommand {
 	public Position targetPosition;
 	public float lifeDuration;
 	public int turn;
-		
 	public SerializableCommand(int index, Vector2 targetPos, float duration, CommandType t, int turn){
 		robotIndex = index;
 		targetPosition = new Position(targetPos);
